a3e585034f9d21fb6b8256e4b3bddbb3d55fe532
Giorgi Tsignadze <giorgi.tsignadze.1@btu.edu.ge>
2025-05-20 16:05:19 +0400

    email form

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
     $('.widget-handle').on('click', function() {
       $('.module-group').toggleClass('active');
     });
+
+    window.onload = function() {
+      $("form").find('input[type="text"], input[type="email"], textarea').val('');
+    };
   });
+
